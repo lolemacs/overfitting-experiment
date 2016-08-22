c80dc9346df8a1f393ca339877e44c81fecb4d49
Savarese <phpsavarese@gmail.com>
2016-08-21 22:31:00 -0300

    Commented some code.

--- generate.py
+++ generate.py
@@ -3,20 +3,24 @@ import numpy as np
 import numpy.polynomial.legendre as leg
 
 def L(x, k):
+    #old
     l = [1, x]
     for i in range(2, k):
         l.append( (2.*i - 1)/i * x * l[-1] - (i - 1.0)/i * l[-2] )
     return np.array(l)
 
 def get_coefs(k):
+    #need to return coefficients that normalize the polynomio
     return np.random.normal(size=k)
 
 def f(x, k):
+    #old
     coefs = get_coefs(k)
     Lq = L(x,k)
     return np.sum(coefs * Lq)
 
 def noise(sigma, n):
+    #return gaussian noise with sigma std
     return np.random.normal(size=n) * sigma
     
 def generate(Q, sigma, N):
