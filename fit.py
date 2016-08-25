48fc435cb7b614d9deeaa64971d0914d225c4120
lolemacs <phpsavarese@gmail.com>
2016-08-25 06:07:08 -0300

    Added plotting

--- fit.py
+++ fit.py
@@ -20,6 +20,7 @@ Z = []
 
 #first experiment, Q fixed as 20
 for N in range(20,120,5):
+    row = []
     for sigma in range(0,200,5):
         data = generate.generate(20, sigma/100., N)
         X = data[0].reshape(N,1)
@@ -31,10 +32,9 @@ for N in range(20,120,5):
         i += 1
         if i in range(0, nIters, nIters/10): print float(i)/nIters
         #print "Err2(in), Err10(in) (%s,%s): " % (N,sigma), err2, err10
-        Z.append(err2 - err10)
+        row.append(err2 - err10)
+    Z.append(row)
 
-
-Z = Z[:, np.newaxis]
 plt.figure(1)
 plt.imshow(Z)
 plt.clim()
