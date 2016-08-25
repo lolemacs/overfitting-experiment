5806b02456a92f7547b4507f86067335780f9043
lolemacs <phpsavarese@gmail.com>
2016-08-25 05:10:25 -0300

    Added plotting

--- fit.py
+++ fit.py
@@ -8,6 +8,17 @@ from sklearn.metrics import f1_score, accuracy_score
 h2 = make_pipeline(PolynomialFeatures(2), LinearRegression())
 h10 = make_pipeline(PolynomialFeatures(10), LinearRegression())
 
+nIters = (120-20)/5 * (200-0)/5
+i = 0
+
+x = np.arange(6)
+y = np.arange(5)
+print y[:, np.newaxis]
+quit()
+
+Z = []
+
+#first experiment, Q fixed as 20
 for N in range(20,120,5):
     for sigma in range(0,200,5):
         data = generate.generate(20, sigma/100., N)
@@ -17,4 +28,15 @@ for N in range(20,120,5):
         h10.fit(X, Y)
         err2 = ((h2.predict(X) - Y)**2).mean()
         err10 = ((h10.predict(X) - Y)**2).mean()
-        print "Err2(in) - Err10(in) (%s,%s): "%(N,sigma), err2 - err10
+        i += 1
+        if i in range(0, nIters, nIters/10): print float(i)/nIters
+        #print "Err2(in), Err10(in) (%s,%s): " % (N,sigma), err2, err10
+        Z.append(err2 - err10)
+
+
+Z = Z[:, np.newaxis]
+plt.figure(1)
+plt.imshow(Z)
+plt.clim()
+plt.title("Boring slide show")
+plt.show()
