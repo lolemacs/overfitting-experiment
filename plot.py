e8a91c4d36fefe2a4635dcc41fc185a177e53ae0
lolemacs <phpsavarese@gmail.com>
2016-09-13 16:49:07 -0300

    Plot script now actually plots

--- plot.py
+++ plot.py
@@ -2,6 +2,8 @@ from os import listdir
 from os.path import isfile, join
 import numpy as np
 import cPickle
+import matplotlib.pyplot as plt
+from matplotlib import cm
 
 fileNames = [f for f in listdir(".") if isfile(join(".", f))]
 Z = []
@@ -9,8 +11,22 @@ for fileName in fileNames:
     if ".pkl" in fileName:
         print fileName
         with open(fileName,"rb") as f:
-            Z.append(cPickle.load(f))
-Z = np.array(Z)
-
-
+            if Z == []: Z = cPickle.load(f)
+            else: Z = np.concatenate((Z,cPickle.load(f)),axis=0)
 print Z.shape
+
+Z = Z.mean(axis=0)
+
+#print Z
+
+Z = np.clip(Z, -.2, .2)
+#print max(dump)
+
+fig, ax = plt.subplots()
+cax = ax.imshow(Z, interpolation='bicubic', cmap=cm.inferno)
+ax.set_title("Things")
+cbar = fig.colorbar(cax, ticks=[-1, 0, 1])
+cbar.ax.set_yticklabels(['0'])
+#plt.xlabel('Sigma')
+#plt.ylabel('N')
+plt.show()
